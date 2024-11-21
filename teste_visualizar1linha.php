88251f5c540fd6ef04a62a0cb39804e2489fc52b
Marcos Vinicius Gomes <131676551+MarcosDrax@users.noreply.github.com>
2024-11-21 18:03:49 -0300

    Update teste_visualizar1linha.php

--- teste_visualizar1linha.php
+++ teste_visualizar1linha.php
@@ -8,7 +8,7 @@
     $arquivo = fopen("visitas_novo.txt", "r");
     while (!feof($arquivo)) { //looping
         $linha_arquivo = fgets($arquivo);
-        for ($i=0; $i < 99; $i++) { //looping
+        for ($i=0; $i < 99; $i++) { 
             if (substr($linha_arquivo,$i,1) == '|') {
                 break;
             } else {
@@ -17,19 +17,25 @@
             $registroatual = substr($linha_arquivo,0,$quantidade);
         }
         $quantidade = 0;                             
-        
+/* preciso adicionar 1 registro por vez mesmo aqueles registros que são iguais*/
+        if (($registroatual !== $registroantecessor) or ($quantidade_vetor == 1)){
 
-        if (($registroatual !== $registroantecessor) && ($quantidade_vetor !== 1)){
-            $conteudolinhas[] = $registroatual;
+            if ($quantidade_vetor == 1){
+                $conteudolinhas
+            }
+            /*agora ele está pegando somente um registro por linha mas ainda aparece os registro duplicados, preciso que ele filtre os registros duplicados */
+
+
+
+            $conteudolinhas[] = $linha_arquivo;
             echo "<pre>";
             print_r($conteudolinhas); 
-            print_r($quantidade_vetor) ;
+            print_r($quantidade_vetor);
             echo "</pre>";
             $conteudolinhas = []; //zera o vetor
+            $registroantecessor = $registroatual;
+            $quantidade_vetor = count($conteudolinhas);
         }
-/* preciso adicionar 1 registro por vez mesmo aqueles registros que são iguais*/
-        $registroantecessor = $registroatual;
-        $quantidade_vetor = count($conteudolinhas);
     }
     fclose($arquivo);    
     ?>
