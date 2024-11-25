71d953e72b7e5432fae1db3eb6a802f8959f5a37
Marcos Vinicius Gomes <131676551+MarcosDrax@users.noreply.github.com>
2024-11-25 13:11:21 -0300

    Update teste_visualizar1linha.php

--- teste_visualizar1linha.php
+++ teste_visualizar1linha.php
@@ -1,36 +1,39 @@
 <?php
-    $quantidade = 0;
-    $quantidade_vetor = 0;
-    $registroatual = '';
-    $registroantecessor = '';
-    $conteudolinhas = []; //vetor
-    $arquivo = fopen("visitas_novo.txt", "r");
+                $quantidade = 0;
+                $quantidade_vetor = 0;
+                $registroatual = '';
+                $registroantecessor = '';
+                $conteudolinhas = []; //vetor
+                $final = false;
+                $arquivo = fopen("visitas_novo.txt", "r");
 
-    while (!feof($arquivo)) { //looping
-        $linha = fgets($arquivo);
-        for ($i=0; $i < 99; $i++) {
-            if (substr($linha,$i,1) == '|') {
-                break;
-            } else {
-                $quantidade = $quantidade+1;
-            }
-            $registroatual = substr($linha,0,$quantidade);
-        }
-        $quantidade = 0;                            
-			
-        if (($registroatual !==  $registroantecessor) and ($quantidade_vetor!== 0)) {           
-            if ($quantidade_vetor == 1) { 
-            echo $conteudolinhas[0] ."_".$quantidade_vetor."</br>";
-            }
-            $conteudolinhas = []; //zera o vetor
-        }
-        $conteudolinhas[]   = $registroatual;
-        $quantidade_vetor   = count($conteudolinhas); 
-        $registroantecessor = $registroatual; 
-        
-        if($registroatual == ' '){
-            break;
-        }
-    }
-    fclose($arquivo);
-    ?>
+                while (true) { //looping
+                    $linha = fgets($arquivo);
+                    
+                    for ($i=0; $i < 99; $i++) {
+                        if (substr($linha,$i,1) == '|') {
+                            break;
+                        } else {
+                            $quantidade = $quantidade+1;
+                        }
+                        $registroatual = substr($linha,0,$quantidade);
+                    }
+                    $quantidade = 0;                            
+                        
+                    if (($registroatual !==  $registroantecessor) and ($quantidade_vetor!== 0)) {           
+                        if ($quantidade_vetor == 1) { 
+                        echo $conteudolinhas[0] ."_".$quantidade_vetor."</br>";
+                        }
+                        $conteudolinhas = []; //zera o vetor
+                    }
+                    $conteudolinhas[]   = $registroatual;
+                    $quantidade_vetor   = count($conteudolinhas); 
+                    $registroantecessor = $registroatual; 
+
+                    if ($linha === false) { 
+                        $final = true;
+                        break;
+                    }
+                }
+                fclose($arquivo);
+            ?>
