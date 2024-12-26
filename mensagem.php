6e454da347496f0360d75fa5a5c5368924e5ce59
Marcos Vinicius Gomes <131676551+MarcosDrax@users.noreply.github.com>
2024-12-26 17:59:11 -0300

    Update mensagem.php

--- mensagem.php
+++ mensagem.php
@@ -65,13 +65,16 @@
                         }
                     }
  
-                    if (!empty($ids_processados[$id])) {
+                    /*if (!empty($ids_processados[$id])) {
                         $conteudolinhas[] = $s; 
                     } else {
                         $ids_processados[$id] = 1; 
                         $conteudolinhas = [$s]; 
-                    }
-                    
+                    }*/
+                            $conteudolinhas[] = $s;
+							$qtd_vetor = count($conteudolinhas);
+
+		
                     $registroantecessor = $id;
                     if ($s === false) break; 
                 }
