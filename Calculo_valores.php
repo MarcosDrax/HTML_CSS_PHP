849fc23195cbcb411acd09fb43b0f58edfe0d5f3
Marcos Vinicius Gomes <131676551+MarcosDrax@users.noreply.github.com>
2025-01-02 11:30:32 -0300

    Update Calculo_valores.php

--- Calculo_valores.php
+++ Calculo_valores.php
@@ -27,82 +27,86 @@
 
                 while (true) {
                     $s = fgets($arquivo);
-                    $quantidade = delimitador($s);
-                    $id = substr($s, 0, $quantidade);
+                    if ($s === false) break; 
+                    $s = trim($s); 
+                    if ($s !== '') {;
+                        $quantidade = delimitador($s);
+                        $id = substr($s, 0, $quantidade);
 
-                    if($id !== $registroantecessor) {/*condição principal*/
-                        if ($qtd_vetor == 2) {
-                            /*vetor = 0 - refaturar o codigo abaixo */
-                            $quantidade = delimitador($conteudolinhas[0]);
-                            $restante = $conteudolinhas[0];
-                            $qtd_id   = delimitador($restante);
-                            $registro = substr($restante, 0, $quantidade);
-                            
-                            $restante = substr($restante, $qtd_id + 1);
-                            $qtd_nome = delimitador($restante);
-                            $nome = substr($restante, 0, $qtd_nome);
-       
-                            $restante = substr($restante, $qtd_nome + 1);
-                            $qtd_id1 = delimitador($restante);
-                            $id_1 = substr($restante, 0, $qtd_id1);
-       
-                            $restante = substr($restante, $qtd_id1 + 1);
-                            $qtd_vlr1 = delimitador($restante);
-                            $valor_1 = substr($restante, 0, $qtd_vlr1);
-                            $valor_1_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_1);
-                            $vlr_1 = number_format(floatval($valor_1_limpo), 2, '.', '');
-       
-                            $restante = substr($restante, $qtd_vlr1 + 1);
-                            $qtd_id2 = delimitador($restante);
-                            $id_2 = substr($restante, 0, $qtd_id2);
-       
-                            $restante = substr($restante, $qtd_id2 + 1);
-                            $qtd_vlr2 = delimitador($restante);
-                            $valor_2 = substr($restante, 0, $qtd_vlr2);
-                            $valor_2_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_2);
-                            $vlr_2 = number_format(floatval($valor_2_limpo), 2, '.', '');
+                        if($id !== $registroantecessor) {/*condição principal*/
+                            if ($qtd_vetor == 2) {
+                                /*vetor = 0 - refaturar o codigo abaixo */
+                                $quantidade = delimitador($conteudolinhas[0]);
+                                $restante = $conteudolinhas[0];
+                                $qtd_id   = delimitador($restante);
+                                $registro = substr($restante, 0, $quantidade);
+                                
+                                $restante = substr($restante, $qtd_id + 1);
+                                $qtd_nome = delimitador($restante);
+                                $nome = substr($restante, 0, $qtd_nome);
+        
+                                $restante = substr($restante, $qtd_nome + 1);
+                                $qtd_id1 = delimitador($restante);
+                                $id_1 = substr($restante, 0, $qtd_id1);
+        
+                                $restante = substr($restante, $qtd_id1 + 1);
+                                $qtd_vlr1 = delimitador($restante);
+                                $valor_1 = substr($restante, 0, $qtd_vlr1);
+                                $valor_1_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_1);
+                                $vlr_1 = number_format(floatval($valor_1_limpo), 2, '.', '');
+        
+                                $restante = substr($restante, $qtd_vlr1 + 1);
+                                $qtd_id2 = delimitador($restante);
+                                $id_2 = substr($restante, 0, $qtd_id2);
+        
+                                $restante = substr($restante, $qtd_id2 + 1);
+                                $qtd_vlr2 = delimitador($restante);
+                                $valor_2 = substr($restante, 0, $qtd_vlr2);
+                                $valor_2_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_2);
+                                $vlr_2 = number_format(floatval($valor_2_limpo), 2, '.', '');
 
-                            /*vetor 1- refaturar o codigo abaixo*/
-                            $quantid_1 = delimitador($conteudolinhas[1]);
-                            $restante1 = $conteudolinhas[1];
-                            $qtd_id_1   = delimitador($restante1);
-                            $registro01 = substr($restante1, 0, $quantid_1);
+                                /*vetor 1- refaturar o codigo abaixo*/
+                                $quantid_1 = delimitador($conteudolinhas[1]);
+                                $restante1 = $conteudolinhas[1];
+                                $qtd_id_1   = delimitador($restante1);
+                                $registro01 = substr($restante1, 0, $quantid_1);
+                                
+                                $restante1 = substr($restante1, $qtd_id + 1);
+                                $qtd_nome1 = delimitador($restante1);
+                                $nome01 = substr($restante1, 0, $qtd_nome);
+        
+                                $restante1 = substr($restante1, $qtd_nome1 + 1);
+                                $ident_1 = delimitador($restante1);
+                                $id_01 = substr($restante1, 0, $ident_1);
+        
+                                $restante1 = substr($restante1, $ident_1 + 1);
+                                $qtd_vlr01 = delimitador($restante1);
+                                $valor_01 = substr($restante1, 0, $qtd_vlr01);
+                                $valor_01_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_01);
+                                $vlr_01 = number_format(floatval($valor_01_limpo), 2, '.', '');
+        
+                                $restante1 = substr($restante1, $qtd_vlr01 + 1);
+                                $qtd_id02 = delimitador($restante1);
+                                $id_02 = substr($restante1, 0, $qtd_id02);
+        
+                                $restante1 = substr($restante1, $qtd_id2 + 1);
+                                $qtd_vlr02 = delimitador($restante1);
+                                $valor_02 = substr($restante1, 0, $qtd_vlr02);
+                                $valor_02_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_02);
+                                $vlr_02 = number_format(floatval($valor_02_limpo), 2, '.', '');
                             
-                            $restante1 = substr($restante1, $qtd_id + 1);
-                            $qtd_nome1 = delimitador($restante1);
-                            $nome01 = substr($restante1, 0, $qtd_nome);
-       
-                            $restante1 = substr($restante1, $qtd_nome1 + 1);
-                            $ident_1 = delimitador($restante1);
-                            $id_01 = substr($restante1, 0, $ident_1);
-       
-                            $restante1 = substr($restante1, $ident_1 + 1);
-                            $qtd_vlr01 = delimitador($restante1);
-                            $valor_01 = substr($restante1, 0, $qtd_vlr01);
-                            $valor_01_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_01);
-                            $vlr_01 = number_format(floatval($valor_01_limpo), 2, '.', '');
-       
-                            $restante1 = substr($restante1, $qtd_vlr01 + 1);
-                            $qtd_id02 = delimitador($restante1);
-                            $id_02 = substr($restante1, 0, $qtd_id02);
-       
-                            $restante1 = substr($restante1, $qtd_id2 + 1);
-                            $qtd_vlr02 = delimitador($restante1);
-                            $valor_02 = substr($restante1, 0, $qtd_vlr02);
-                            $valor_02_limpo = str_replace(['R$', ',', ' '], ['', '.', ''], $valor_02);
-                            $vlr_02 = number_format(floatval($valor_02_limpo), 2, '.', '');
-                          
-                            echo  $conteudolinhas[0]."<br>";
-                            echo  "valor total = ".$vlr_1 + $vlr_2."<br>";
-                            echo  $conteudolinhas[1]."<br>";
-                            echo  "valor total = ".$vlr_01+ $vlr_02 ."<br>";
+                                $vlrTot01 = $vlr_1 + $vlr_2;
+                                $vlrTot02 = $vlr_01 + $vlr_02;
+                                echo  $conteudolinhas[0]."valor total = ".$vlrTot01."<br>";
+                                echo  $conteudolinhas[1]."valor total = ".$vlrTot02 ."<br>";
+                            }
+                            $conteudolinhas = [];
                         }
-                        $conteudolinhas = [];
+                        $conteudolinhas[] = $s;
+                        $qtd_vetor = count($conteudolinhas);
+                        $registroantecessor = $id;
+                        //if ($s === false) break;
                     }
-                    $conteudolinhas[] = $s;
-                    $qtd_vetor = count($conteudolinhas);
-                    $registroantecessor = $id;
-                    if ($s === false) break;
                 }
                 fclose($arquivo);
             ?>
